6053f9bfd2ac5ecdbce0ad8c22c4b76c3f821323
Dean Camera <dean@fourwalledcubicle.com>
2014-01-07 15:31:55 +1100

    Initial commit.

---
+++ Template/Template.c
@@ -0,0 +1,13 @@
+/*
+              DMBS Build System
+      Copyright (C) Dean Camera, 2014.
+
+   dean [at] fourwalledcubicle [dot] com
+         www.fourwalledcubicle.com
+
+ */
+
+int main(void)
+{
+	// Application code here.
+}
