2d9314cc7707c6b70f0ce15cfce309bf943e9ce3
NicoHood <git@nicohood.de>
2018-03-17 14:24:47 +0100

    Added DMBS_ARCH_ define

--- DMBS/gcc.mk
+++ DMBS/gcc.mk
@@ -117,7 +117,7 @@ else ifneq ($(findstring $(ARCH), UC3),)
 endif
 BASE_CC_FLAGS += -Wall -fno-strict-aliasing -funsigned-char -funsigned-bitfields -ffunction-sections
 BASE_CC_FLAGS += -I.
-BASE_CC_FLAGS += -DARCH=ARCH_$(ARCH)
+BASE_CC_FLAGS += -DARCH=ARCH_$(ARCH) -DDMBS_ARCH_$(ARCH)
 ifneq ($(F_CPU),)
    BASE_CC_FLAGS += -DF_CPU=$(F_CPU)UL
 endif
